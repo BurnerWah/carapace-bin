6bc09266d32582bc5a5eb761c0f25e4b8125d98e
Burner <10004812+BurnerWah@users.noreply.github.com>
2025-01-16 22:35:54 -0600

    xsv: wrote cat cmd

--- completers/xsv_completer/cmd/cat.go
+++ completers/xsv_completer/cmd/cat.go
@@ -14,10 +14,15 @@ var catCmd = &cobra.Command{
 func init() {
 	carapace.Gen(catCmd).Standalone()
 
-	catCmd.Flags().StringP("delimiter", "d", "", "The field delimiter for reading CSV data.")
-	catCmd.Flags().BoolP("help", "h", false, "Display this message")
-	catCmd.Flags().BoolP("no-headers", "n", false, "When set, the first row will NOT be interpreted")
-	catCmd.Flags().StringP("output", "o", "", "Write output to <file> instead of stdout.")
-	catCmd.Flags().BoolP("pad", "p", false, "When concatenating columns, this flag will cause")
+	catCmd.Flags().StringP("delimiter", "d", "", "The field delimiter for reading CSV data")
+	catCmd.Flags().BoolP("no-headers", "n", false, "Don't interpert the first row as a header")
+	catCmd.Flags().StringP("output", "o", "", "Write output to a file instead of stdout")
+	catCmd.Flags().BoolP("pad", "p", false, "Pad rows if data isn't long enough, or show all records for columns")
 	rootCmd.AddCommand(catCmd)
+
+	carapace.Gen(catCmd).FlagCompletion(carapace.ActionMap{
+		"output": carapace.ActionFiles(),
+	})
+	carapace.Gen(catCmd).PositionalCompletion(carapace.ActionValues("rows", "columns"))
+	carapace.Gen(catCmd).PositionalAnyCompletion(carapace.ActionFiles(".csv"))
 }
