ac59f0d03e6510d560437234217aea2acc22cd1e
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-09 13:49:13 -0600

    Finished 'tailscale switch'

--- completers/common/tailscale_completer/cmd/switch.go
+++ completers/common/tailscale_completer/cmd/switch.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/carapace-sh/carapace"
 	"github.com/spf13/cobra"
 )
@@ -15,6 +17,18 @@ func init() {
 	carapace.Gen(switchCmd).Standalone()
 
 	switchCmd.Flags().Bool("list", false, "list available accounts")
+
+	carapace.Gen(switchCmd).PositionalCompletion(carapace.ActionExecCommand("tailscale", "switch", "--list")(func(output []byte) carapace.Action {
+		lines := strings.Split(string(output), "\n")
+		vals := make([]string, 0)
+
+		for _, line := range lines {
+			if fields := strings.Fields(line); len(fields) > 1 && fields[0] != "ID" {
+				vals = append(vals, fields[0], fields[2])
+			}
+		}
+		return carapace.ActionValuesDescribed(vals...)
+	}))
 
 	rootCmd.AddCommand(switchCmd)
 }
