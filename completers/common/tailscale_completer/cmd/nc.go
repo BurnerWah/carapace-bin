e11f11bc34a377def499399ce28bbb9acd036686
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-09 14:36:29 -0600

    Added incomplete 'tailscale nc'

---
+++ completers/common/tailscale_completer/cmd/nc.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bin/completers/common/tailscale_completer/cmd/action"
+	"github.com/carapace-sh/carapace-bin/pkg/actions/net"
+	"github.com/spf13/cobra"
+)
+
+var ncCmd = &cobra.Command{
+	Use:   "nc",
+	Short: "Connect to a port on a host, connected to stdin/stdout",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(ncCmd).Standalone()
+
+	rootCmd.AddCommand(ncCmd)
+
+	carapace.Gen(ncCmd).PositionalCompletion(
+		action.ActionPeers(), // TODO: filter out mullvad exit nodes
+		net.ActionKnownPorts(),
+	)
+}
