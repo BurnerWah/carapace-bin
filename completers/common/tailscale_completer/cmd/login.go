fb4210f2fb9e705621f60fed6a8174f07f869c46
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-09 13:10:40 -0600

    Added `tailscale login`

---
+++ completers/common/tailscale_completer/cmd/login.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bin/completers/common/tailscale_completer/cmd/common"
+	"github.com/spf13/cobra"
+)
+
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "Log in to a Tailscale account",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(loginCmd).Standalone()
+
+	common.AddSettingsFlags(loginCmd)
+	common.AddUpFlags(loginCmd)
+
+	loginCmd.Flags().String("nickname", "", "nickname for the current account")
+
+	rootCmd.AddCommand(loginCmd)
+}
