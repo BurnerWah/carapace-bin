09634a269784013485877c43413ef3817741f571
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-09 18:55:48 -0600

    Added `tailscale cert`

---
+++ completers/common/tailscale_completer/cmd/cert.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/spf13/cobra"
+)
+
+var certCmd = &cobra.Command{
+	Use:   "cert",
+	Short: "Get TLS certs",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(certCmd).Standalone()
+
+	certCmd.Flags().String("cert-file", "", "output cert file or \"-\" for stdout")
+	certCmd.Flags().String("key-file", "", "output key file or \"-\" for stdout")
+	certCmd.Flags().Duration("min-validity", 0, "ensure the certificate is valid for at least this duration")
+	certCmd.Flags().Bool("serve-demo", false, "if true, serve on port :443 using the cert as a demo, instead of writing out the files to disk")
+
+	rootCmd.AddCommand(certCmd)
+
+	carapace.Gen(certCmd).FlagCompletion(carapace.ActionMap{
+		"cert-file": carapace.ActionFiles(),
+		"key-file":  carapace.ActionFiles(),
+	})
+}
